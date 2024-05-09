303e5d77b34e1fbd5744efa5f2768824ea342ca2
wailman24 <133514103+wailmans@users.noreply.github.com>
2024-05-09 22:19:19 +0200

    the first time

---
+++ src/immoblier_Project/bien_CRUD.java
@@ -0,0 +1,43 @@
+package immoblier_Project;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class bien_CRUD {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					bien_CRUD window = new bien_CRUD();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public bien_CRUD() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
